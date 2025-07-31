d4aa950401b78764010be42a5e06d586b6ff4edd
Maurice <mcornejoalv@unsa.edu.pe>
2025-07-31 05:20:39 -0500

    vector.h and sort.h added

---
+++ include/sort.h
@@ -0,0 +1,47 @@
+// sort.h
+#ifndef __SORT_H__
+#define __SORT_H__
+
+#include "vector.h"
+
+template <typename T>
+void merge(Vector<T> &arr, int left, int mid, int right, bool (*compare)(const T &, const T &)) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    Vector<T> L(n1), R(n2);
+    for (int i = 0; i < n1; ++i)
+        L.push_back(arr[left + i]);
+    for (int j = 0; j < n2; ++j)
+        R.push_back(arr[mid + 1 + j]);
+
+    int i = 0, j = 0, k = left;
+    while (i < L.getSize() && j < R.getSize()) {
+        if (compare(L[i], R[j])) {
+            arr[k++] = L[i++];
+        } else {
+            arr[k++] = R[j++];
+        }
+    }
+
+    while (i < L.getSize()) arr[k++] = L[i++];
+    while (j < R.getSize()) arr[k++] = R[j++];
+}
+
+template <typename T>
+void mergeSort(Vector<T> &arr, int left, int right, bool (*compare)(const T &, const T &)) {
+    if (left < right) {
+        int mid = (left + right) / 2;
+        mergeSort(arr, left, mid, compare);
+        mergeSort(arr, mid + 1, right, compare);
+        merge(arr, left, mid, right, compare);
+    }
+}
+
+template <typename T>
+void sort(Vector<T> &arr, bool (*compare)(const T &, const T &)) {
+    if (arr.getSize() > 1)
+        mergeSort(arr, 0, arr.getSize() - 1, compare);
+}
+
+#endif
---
+++ include/vector.h
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+template <class T>
+class Vector {
+private:
+    T *data;
+    size_t capacity;
+    size_t size;
+public:
+    Vector(size_t initial_capacity = 10) : capacity(initial_capacity), size(0) {
+        data = new T[capacity];
+    }
+    ~Vector() {
+        delete[] data;
+    }
+    void push_back(const T &value) {
+        if (size >= capacity) {
+            capacity *= 2;
+            T *new_data = new T[capacity];
+            for (size_t i = 0; i < size; i++) {
+                new_data[i] = data[i];
+            }
+            delete[] data;
+            data = new_data;
+        }
+        data[size++] = value;
+    }
+    void pop_back() {
+        if (size > 0) {
+            size--;
+        }
+    }
+    T &operator[](size_t index) {
+        return data[index];
+    }
+    const T &operator[](size_t index) const {
+        return data[index];
+    }
+    size_t getSize() const {
+        return size;
+    }
+    size_t getCapacity() const {
+        return capacity;
+    }
+    void clear() {
+        size = 0;
+    }
+};
